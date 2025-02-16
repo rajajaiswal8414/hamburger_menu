44c32dd3f10b7feeaf0f8d62fe01413596fc0d92
rajajais8414 <143202200+rajajaiswal8414@users.noreply.github.com>
2025-02-16 18:35:44 +0530

    Updated menu functionality

--- src/script.js
+++ src/script.js
@@ -2,15 +2,22 @@ const menuBox = document.querySelector('.menu-box');
 const humburgerIcon = document.querySelector('.humburgerIcon');
 const menuIcon = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
+const goToTop = document.querySelector('.goToTop');
 
-
-humburgerIcon.addEventListener('click', () => {
+humburgerIcon.addEventListener('click', (e) => {
+    // e.stopPropagation()
     menuBox.classList.add('show')
     menuIcon.classList.add('d-none');
     closeIcon.classList.remove('d-none')
 })
 
 closeIcon.addEventListener('click', () => {
+    menuBox.classList.remove('show');
+    menuIcon.classList.remove('d-none');
+    closeIcon.classList.add('d-none');
+})
+
+window.addEventListener('click', (e) => {
     menuBox.classList.remove('show');
     menuIcon.classList.remove('d-none');
     closeIcon.classList.add('d-none');
